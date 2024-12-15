d579d12da9c084a5ae81f24f3d9e5338c0899e99
kumarmahesh3023 <73269819+kumarmahesh3023@users.noreply.github.com>
2024-12-16 04:43:05 +0530

    - dogBreedBuddy implemented

---
+++ DogBreedBuddy/Utility/Notification+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Extension.swift
+//  DogBreedBuddy
+//
+//  Created by Mahesh Kumar on 15/12/24.
+//
+
+import Foundation
+
+///Notification center observer keys
+extension Notification.Name {
+    static let favouriteListUpdated = Notification.Name("favouriteListUpdated")
+}
